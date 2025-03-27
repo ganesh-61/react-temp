3675fe20077f822c5973fb63eb4d15931921494f
ganesh-61 <vvpsaiganesh@gmail.com>
2025-03-27 12:31:00 +0530

    add

--- src/App.jsx
+++ src/App.jsx
@@ -1,39 +1,34 @@
 import { useState } from "react";
-import "./App.css";
+// import "./App.css";
 import { BrowserRouter, Link,Routes,Route } from "react-router-dom";
 import App1 from "./components/App1";
 import App2 from "./components/App2";
 import App3 from "./components/App3";
 import App4 from "./components/App4";
-import App5 from "./components/App5";
+import App5 from "./components/App5"
 import App6 from "./components/App6";
 import App7 from "./components/App7";
 import App8 from "./components/App8";
 import App9 from "./components/App9";
 import App10 from "./components/App10";
 import App11 from "./components/App11";
+import App12 from "./components/App12";
 function App() {
   return (
     <BrowserRouter>
       <div>
-
-        <Link to="app1">App1</Link>
-        <Link to="app2">App2</Link>
-        <Link to="app3">App3</Link>
-        <Link to="app4">App4</Link>
-        <Link to="app5">App5</Link>
-        <Link to="app6">App6</Link>
-        <Link to="app7">App7</Link>
-        <Link to="app8">App8</Link>
-        <Link to="app8">App8</Link>
-        <Link to="app8">App8</Link>
-        <Link to="app8">App8</Link>
-        <Link to="app9">App9</Link>
-        <Link to="app10">App10</Link>
-        <Link to="app11">App11</Link>
-
-
-
+        <Link to="app1">App1</Link>-
+        <Link to="app2">App2</Link>-
+        <Link to="app3">App3</Link>-
+        <Link to="app4">App4</Link>-
+        <Link to="app5">App5</Link>-
+        <Link to="app6">App6</Link>-
+        <Link to="app7">App7</Link>-
+        <Link to="app8">App8</Link>-
+        <Link to="app9">App9</Link>-
+        <Link to="app10">App10</Link>-
+        <Link to="app11">App11</Link>-
+        <Link to="app12">App12</Link>
       </div>
       <hr></hr>
       <div>
@@ -50,8 +45,7 @@ function App() {
           <Route path="app9" element={<App9 />} />
           <Route path="app10" element={<App10 />} />
           <Route path="app11" element={<App11 />} />
-
-
+          <Route path="app12" element={<App12 />} />
         </Routes>
       </div>
     </BrowserRouter>
--- src/components/App10.jsx
+++ src/components/App10.jsx
@@ -1,16 +1,24 @@
-import React, { useState } from "react";
+import React from "react";
+import { useState, useEffect, useRef } from "react";
 export default function App10() {
-    const [number,setNumber] = useState();
-    return (
-        <div>
-           <input 
-           type="number"
-           placeholder="enter a number"
-           onChange={(e) => setNumber(e.target.value)}
-           ></input>
-           <div>
-            {number}
-            </div>
-        </div>
-    )
+  const [num, setNum] = useState(0);
+  const divRef = useRef();
+  const numRef = useRef(0);
+  useEffect(() => {
+    if (numRef.current > num) {
+      divRef.current.style.color = "red";
+    } else {
+      divRef.current.style.color = "green";
+    }
+    numRef.current = num;
+  }, [num]);
+  return (
+    <div>
+      <h3>This is App10</h3>
+      <p>
+        <input type="number" onChange={(e) => setNum(e.target.value)}></input>
+      </p>
+      <div ref={divRef}>{numRef.current}-{num}</div>
+    </div>
+  );
 }
