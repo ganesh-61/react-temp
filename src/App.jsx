8ee86bdb6bd097f0470d2e5af416969e6e11d88f
ganesh-61 <vvpsaiganesh@gmail.com>
2025-03-27 13:44:49 +0530

    add

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,11 @@
 import { useState } from "react";
-// import "./App.css";
+import "./App.css";
 import { BrowserRouter, Link,Routes,Route } from "react-router-dom";
 import App1 from "./components/App1";
 import App2 from "./components/App2";
 import App3 from "./components/App3";
 import App4 from "./components/App4";
-import App5 from "./components/App5"
+import App5 from "./components/App5";
 import App6 from "./components/App6";
 import App7 from "./components/App7";
 import App8 from "./components/App8";
@@ -17,18 +17,25 @@ function App() {
   return (
     <BrowserRouter>
       <div>
-        <Link to="app1">App1</Link>-
-        <Link to="app2">App2</Link>-
-        <Link to="app3">App3</Link>-
-        <Link to="app4">App4</Link>-
-        <Link to="app5">App5</Link>-
-        <Link to="app6">App6</Link>-
-        <Link to="app7">App7</Link>-
-        <Link to="app8">App8</Link>-
-        <Link to="app9">App9</Link>-
-        <Link to="app10">App10</Link>-
-        <Link to="app11">App11</Link>-
+
+        <Link to="app1">App1</Link>
+        <Link to="app2">App2</Link>
+        <Link to="app3">App3</Link>
+        <Link to="app4">App4</Link>
+        <Link to="app5">App5</Link>
+        <Link to="app6">App6</Link>
+        <Link to="app7">App7</Link>
+        <Link to="app8">App8</Link>
+        <Link to="app8">App8</Link>
+        <Link to="app8">App8</Link>
+        <Link to="app8">App8</Link>
+        <Link to="app9">App9</Link>
+        <Link to="app10">App10</Link>
+        <Link to="app11">App11</Link>
         <Link to="app12">App12</Link>
+
+
+
       </div>
       <hr></hr>
       <div>
@@ -46,6 +53,8 @@ function App() {
           <Route path="app10" element={<App10 />} />
           <Route path="app11" element={<App11 />} />
           <Route path="app12" element={<App12 />} />
+
+
         </Routes>
       </div>
     </BrowserRouter>
